d762bb3bfccc46e859fa80beadd375ca656e9ab4
Leo <leopichol1307@gmail.com>
2026-06-04 15:09:54 -0300

    Preparando ambiente para deploy

--- app.py
+++ app.py
@@ -113,8 +113,6 @@ def login():
         session["logado"] = True
         return redirect("/")
     return "Usuário ou senha inválidos"
-
+crud.criar_tabela()
 if __name__ == "__main__":
     app.run(debug=True)
-
-crud.criar_tabela()
